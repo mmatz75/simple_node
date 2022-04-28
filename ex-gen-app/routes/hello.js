4666bbd43412a2f4b111bb80922f28a43b9c501f
mmatz75 <69707077+mmatz75@users.noreply.github.com>
2022-04-28 22:58:20 +0900

    added session

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -2,9 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
+  let msg = '何か書いて送信してください。'
+  if (req.session.message != undefined) {
+    msg = 'Last Message: ' + req.session.message
+  }
+  let data = {
+    title: 'Hello!',
+    content: msg,
+  }
+  res.render('hello', data)
+})
+
+router.post('/post', (req, res, next) => {
+  let msg = req.body['message']
+  req.session.message = msg
   let data = {
     title: 'Helo!',
-    content: 'これは、サンプルのコンテンツです。<br>This is a sample content.',
+    content: 'Last Message: ' + req.session.message,
   }
   res.render('hello', data)
 })
