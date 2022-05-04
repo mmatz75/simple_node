9f93befc27ec94b7724cb642775eda4dc50dad72
mmatz75 <69707077+mmatz75@users.noreply.github.com>
2022-05-04 21:07:36 +0900

    search by Sequelize

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const db = require('../models/index')
+const { Op } = require('sequelize')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+router.get('/', (req, res, next) => {
+  const nm = req.query.name
+  const ml = req.query.mail
+  db.User.findAll({
+    where: {
+      [Op.or]: [
+        {
+          name: { [Op.like]: '%' + nm + '%' },
+        },
+        {
+          mail: { [Op.like]: '%' + ml + '%' },
+        },
+      ],
+    },
+  }).then((usrs) => {
+    let data = {
+      title: 'Users/Index',
+      content: usrs,
+    }
+    res.render('users/index', data)
+  })
+})
+module.exports = router
