cd80a37a4bc0cf68b2f6533d0323f43436401e31
mmatz75 <69707077+mmatz75@users.noreply.github.com>
2022-05-06 00:04:33 +0900

    CRUD by sequelize

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -7,18 +7,7 @@ const { Op } = require('sequelize')
 router.get('/', (req, res, next) => {
   const nm = req.query.name
   const ml = req.query.mail
-  db.User.findAll({
-    where: {
-      [Op.or]: [
-        {
-          name: { [Op.like]: '%' + nm + '%' },
-        },
-        {
-          mail: { [Op.like]: '%' + ml + '%' },
-        },
-      ],
-    },
-  }).then((usrs) => {
+  db.User.findAll().then((usrs) => {
     let data = {
       title: 'Users/Index',
       content: usrs,
@@ -26,4 +15,76 @@ router.get('/', (req, res, next) => {
     res.render('users/index', data)
   })
 })
+
+// add
+router.get('/add', (req, res, next) => {
+  let data = {
+    title: 'Users/Add',
+  }
+  res.render('users/add', data)
+})
+router.post('/add', (req, res, next) => {
+  db.sequelize
+    .sync()
+    .then(() =>
+      db.User.create({
+        name: req.body.name,
+        pass: req.body.pass,
+        mail: req.body.mail,
+        age: req.body.age,
+      })
+    )
+    .then((usr) => {
+      res.redirect('/users')
+    })
+})
+
+// edit
+router.get('/edit', (req, res, next) => {
+  db.User.findByPk(req.query.id).then((usr) => {
+    let data = {
+      title: 'Users/Edit',
+      form: usr,
+    }
+    res.render('users/edit', data)
+  })
+})
+router.post('/edit', (req, res, next) => {
+  db.sequelize
+    .sync()
+    .then(() =>
+      db.User.update(
+        {
+          name: req.body.name,
+          pass: req.body.pass,
+          mail: req.body.mail,
+          age: req.body.age,
+        },
+        { where: { id: req.body.id } }
+      )
+    )
+    .then((usr) => {
+      res.redirect('/users')
+    })
+})
+
+// delete
+router.get('/delete', (req, res, next) => {
+  db.User.findByPk(req.query.id).then((usr) => {
+    let data = {
+      title: 'Users/Delete',
+      form: usr,
+    }
+    res.render('users/delete', data)
+  })
+})
+router.post('/delete', (req, res, next) => {
+  db.sequelize
+    .sync()
+    .then(() => db.User.destroy({ where: { id: req.body.id } }))
+    .then((usr) => {
+      res.redirect('/users')
+    })
+})
+
 module.exports = router
