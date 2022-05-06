a5a8a3896328d86fd13b0a8ccbba5698574f5599
mmatz75 <69707077+mmatz75@users.noreply.github.com>
2022-05-07 00:54:07 +0900

    convert to board-app

---
+++ board-app/models/board.js
@@ -0,0 +1,31 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  const Board = sequelize.define(
+    'Board',
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        validate: {
+          notEmpty: {
+            msg: '利用者を入力してください。',
+          },
+        },
+      },
+      message: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: 'メッセージを入力してください。',
+          },
+        },
+      },
+    },
+    {}
+  )
+
+  Board.associate = (models) => {
+    Board.belongTo(models.User)
+  }
+  return Board
+}
---
+++ board-app/models/user.js
@@ -0,0 +1,52 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: '名前を入力してください。',
+          },
+        },
+      },
+      pass: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: 'パスワードを入力してください。',
+          },
+        },
+      },
+      mail: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            msg: 'メールアドレスを入力してください。',
+          },
+        },
+      },
+      age: {
+        type: DataTypes.INTEGER,
+        validate: {
+          isInt: {
+            msg: '整数を入力してください。',
+          },
+          min: {
+            args: [0],
+            msg: 'ゼロ以上の値を入力してください。',
+          },
+        },
+      },
+    },
+    {}
+  )
+
+  User.associate = (models) => {
+    User.hasMany(models.Board)
+  }
+
+  return User
+}
