386dc6b0d252c8b40db76bbdedabcfdd3738a9ca
mmatz75 <69707077+mmatz75@users.noreply.github.com>
2022-05-09 00:05:05 +0900

    board app

--- board-app/routes/users.js
+++ board-app/routes/users.js
@@ -1,11 +1,22 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+const db = require('../models/index')
+const { Op } = require('sequelize')
 
 /* GET users listing. */
-// router.get('/', function (req, res, next) {
-//   res.send('respond with a resource')
-// })
+router.get('/', (req, res, next) => {
+  const nm = req.query.name
+  const ml = req.query.mail
+  db.User.findAll().then((usrs) => {
+    let data = {
+      title: 'Users/Index',
+      content: usrs,
+    }
+    res.render('users/index', data)
+  })
+})
 
+// login
 router.get('/login', (req, res, next) => {
   let data = {
     title: 'Users/Login',
@@ -15,7 +26,7 @@ router.get('/login', (req, res, next) => {
 })
 
 router.post('/login', (req, res, next) => {
-  dbn.User.findOne({
+  db.User.findOne({
     where: {
       name: req.body.name,
       pass: req.body.pass,
@@ -36,6 +47,86 @@ router.post('/login', (req, res, next) => {
       res.render('users/login', data)
     }
   })
+})
+
+// add
+router.get('/add', (req, res, next) => {
+  let data = {
+    title: 'Users/Add',
+    form: new db.User(),
+    err: null,
+  }
+  res.render('users/add', data)
+})
+router.post('/add', (req, res, next) => {
+  const form = {
+    name: req.body.name,
+    pass: req.body.pass,
+    mail: req.body.mail,
+    age: req.body.age,
+  }
+  db.sequelize
+    .sync()
+    .then(() => db.User.create(form))
+    .then((usr) => {
+      res.redirect('/users')
+    })
+    .catch((err) => {
+      let data = {
+        title: 'Users/Add',
+        form: form,
+        err: err,
+      }
+      res.render('users/add', data)
+    })
+})
+
+// edit
+router.get('/edit', (req, res, next) => {
+  db.User.findByPk(req.query.id).then((usr) => {
+    let data = {
+      title: 'Users/Edit',
+      form: usr,
+    }
+    res.render('users/edit', data)
+  })
+})
+router.post('/edit', (req, res, next) => {
+  db.sequelize
+    .sync()
+    .then(() =>
+      db.User.update(
+        {
+          name: req.body.name,
+          pass: req.body.pass,
+          mail: req.body.mail,
+          age: req.body.age,
+        },
+        { where: { id: req.body.id } }
+      )
+    )
+    .then((usr) => {
+      res.redirect('/users')
+    })
+})
+
+// delete
+router.get('/delete', (req, res, next) => {
+  db.User.findByPk(req.query.id).then((usr) => {
+    let data = {
+      title: 'Users/Delete',
+      form: usr,
+    }
+    res.render('users/delete', data)
+  })
+})
+router.post('/delete', (req, res, next) => {
+  db.sequelize
+    .sync()
+    .then(() => db.User.destroy({ where: { id: req.body.id } }))
+    .then((usr) => {
+      res.redirect('/users')
+    })
 })
 
 module.exports = router
